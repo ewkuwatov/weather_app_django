3787ce04629e67ae7b6f12bbcfac02d053bbe012
Your Name <you@example.com>
2024-03-07 11:56:14 +0500

    fix errors

--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
+# from django.shortcuts import render
+# import requests
+#
+# def get_weather(city):
+#     api_key = '65ba6faa407e4dfb9664b0f3b1ef1235'
+#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+#     response = requests.get(url)
+#     data = response.json()
+#     return data
+#
+# def get_currency_rates():
+#     url = 'https://cbu.uz/ru/arkhiv-kursov-valyut/json/'
+#     response = requests.get(url)
+#     data = response.json()
+#     return data
+#
+# def home(request):
+#     if request.method == 'POST':
+#         city = request.POST.get('city')
+#         weather_data = get_weather(city)
+#         currency_data = get_currency_rates()
+#
+#         return render(request, 'app/index.html', {'weather_data': weather_data, 'currency_data': currency_data})
+#     else:
+#         return render(request, 'app/index.html')
 from django.shortcuts import render
 import requests
-
 
 def get_weather(city):
     api_key = '65ba6faa407e4dfb9664b0f3b1ef1235'
@@ -10,26 +34,21 @@ def get_weather(city):
     return data
 
 def home(request):
+    # Список кодов валют, которые вы хотите показать
+    currency_codes = ['USD', 'EUR', 'RUB', 'GBP', 'JPY', 'AZN']
+
+    # Получение данных о погоде
     if request.method == 'POST':
         city = request.POST.get('city')
         weather_data = get_weather(city)
-
-        url = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/"
-        response = requests.get(url)
-        data = response.json()
-
-        # Обработка данных и выбор нужных вам валют
-        currencies = {
-            "US Dollar": data[0]["Rate"],
-            "Euro": data[1]["Rate"],
-            "Ruble": data[2]["Rate"],
-            "Pound Sterling": data[3]["Rate"],
-            "Japan Yen": data[4]["Rate"],
-            "Argentine Peso": data[5]["Rate"],
-        }
-        return render(request, 'app/index.html', {'weather_data': weather_data,
-                                                                     'currencies': currencies})
     else:
-        return render(request, 'app/index.html')
+        weather_data = None
 
+    # Получение данных о курсах валют из JSON файла
+    response = requests.get('https://cbu.uz/ru/arkhiv-kursov-valyut/json/')
+    currencies = response.json()
 
+    # Фильтрация списка валют по кодам, которые мы хотим показать
+    filtered_currencies = [currency for currency in currencies if currency['Ccy'] in currency_codes]
+
+    return render(request, 'app/index.html', {'weather_data': weather_data, 'currencies': filtered_currencies})
